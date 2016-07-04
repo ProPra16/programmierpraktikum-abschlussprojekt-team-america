21a2cca553cd9e75461f195c4842fc073b1d80e4
htd7 <hoang.tran.dinh@uni-duesseldorf.de>
2016-07-04 14:48:29 +0200

    testHello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) { 
-        System.out.println("Hello!");
+	double n = 3; 
+        System.out.println("Hello!, World");
+	String Morris = "Morris";
     }
 }
